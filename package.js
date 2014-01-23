e307edf5d105da19570096a03e031ad7c8e9d69f
pahans <pahan123@gmail.com>
2014-01-23 22:36:08 +0530

    changed namespace to InlineHelp

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function(api, where) {
   api.use(['handlebars','templating', 'showdown'], 'client');
 
-  api.export('Help', ['client']);
+  api.export('InlineHelp', ['client']);
 
   api.add_files([
     'lib/popover.html','lib/popover.css','lib/inline-help.js'
